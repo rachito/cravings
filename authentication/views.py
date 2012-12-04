15b595f28c37e53779e2117b122cc04784997521
Rajiv Rivero <rajiv.rivero@gmail.com>
2012-12-03 19:30:54 -0600

    Documentacion de la vista de autenticación.

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 def twitter_login():
+    """ Vista para el login con twitter """
     return render_to_response(
         'authentication/twitter_login.html', {},
         context_instance=RequestContext(request)
